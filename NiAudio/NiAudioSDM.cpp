1714621b31efe340bdc56bb37c3455de746bec2b
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2026-03-27 21:24:54 +0100

    Initial reboot (v2.6)

---
+++ NiAudio/NiAudioSDM.cpp
@@ -0,0 +1,49 @@
+// EMERGENT GAME TECHNOLOGIES PROPRIETARY INFORMATION
+//
+// This software is supplied under the terms of a license agreement or
+// nondisclosure agreement with Emergent Game Technologies and may not 
+// be copied or disclosed except in accordance with the terms of that 
+// agreement.
+//
+//      Copyright (c) 1996-2008 Emergent Game Technologies.
+//      All Rights Reserved.
+//
+// Emergent Game Technologies, Chapel Hill, North Carolina 27517
+// http://www.emergent.net
+
+// Precompiled Header
+#include "NiAudioPCH.h"
+#include "NiAudio.h"
+#include "NiAudioSDM.h"
+
+NiImplementSDMConstructor(NiAudio, "NiMain NiSystem");
+
+#ifdef NIAUDIO_EXPORT
+NiImplementDllMain(NiAudio);
+#endif
+
+//---------------------------------------------------------------------------
+void NiAudioSDM::Init()
+{
+    NiImplementSDMInitCheck();
+
+    NiRegisterStream(NiAudioListener);
+    NiRegisterStream(NiAudioSource);
+    NiRegisterStream(NiAudioSystem);
+
+    //NiAudioSystem::ms_pAudioSystem = (NiAudioSystem*)NiNew NiAudioSystem;
+    //NIASSERT(NiAudioSystem::ms_pAudioSystem);
+}
+//---------------------------------------------------------------------------
+void NiAudioSDM::Shutdown()
+{
+    NiImplementSDMShutdownCheck();
+
+    NiUnregisterStream(NiAudioListener);
+    NiUnregisterStream(NiAudioSource);
+    NiUnregisterStream(NiAudioSystem);
+
+    //NiDelete NiAudioSystem::ms_pAudioSystem;
+    //NiAudioSystem::ms_pAudioSystem = NULL;
+}
+//---------------------------------------------------------------------------
--- vcpkg_install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Required for DX9 and DX10 project.
-vcpkg install dxsdk-d3dx:x86-windows
-rem If you not linked vcpkg yet.
-vcpkg integrate install
-pause
