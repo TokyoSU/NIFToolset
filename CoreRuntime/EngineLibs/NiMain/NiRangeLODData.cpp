e15c22b4b3fc6bc0c1edb73a480c98e372a12c66
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2026-07-06 20:02:01 +0200

    Update NiRangeLODData.cpp
    - Only use horizontal for LOD test.

--- CoreRuntime/EngineLibs/NiMain/NiRangeLODData.cpp
+++ CoreRuntime/EngineLibs/NiMain/NiRangeLODData.cpp
@@ -159,6 +159,7 @@ int NiRangeLODData::GetLODLevel(const NiCamera* pkCamera, NiLODNode* pkLOD) cons
         GetAggregateChildBoundCenter(pkLOD, kWorldCenter);
 
     NiPoint3 kDiff = kWorldCenter - pkCamera->GetWorldLocation();
+    kDiff.z = 0.0f;
     float fDist = kDiff.Length() * pkCamera->GetLODAdjust();
 
     for (unsigned int iLODLevel = 0; iLODLevel < m_uiNumRanges; iLODLevel++)
